c839b594e4d19216fb2356f0a86844e608101c1f
PJ Reiniger <pj.reiniger@gmail.com>
2024-01-03 11:25:59 -0500

    Regenerate (#10)

--- generate/get_phoenix6_dependencies.py
+++ generate/get_phoenix6_dependencies.py
@@ -133,9 +133,7 @@ def get_phoenix6_dependencies(
     group.add_java_meta_dependency(
         "wpiapi-java",
         group_id=f"com.ctre.phoenix6",
-        deps=[
-            "wpilibj-java",
-        ],
+        deps=["wpilibj-java", "phoenix6-hal"],
     )
 
     return group
