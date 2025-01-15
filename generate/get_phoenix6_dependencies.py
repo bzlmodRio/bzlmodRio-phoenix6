0a00d7306a0e7335167cef2c438d3f54630995d2
PJ Reiniger <pj.reiniger@gmail.com>
2025-01-15 03:16:15 -0500

    Auto Update to '25.2.0' (#38)
    Co-authored-by: pjreiniger <10381694+pjreiniger@users.noreply.github.com>

--- generate/get_phoenix6_dependencies.py
+++ generate/get_phoenix6_dependencies.py
@@ -51,6 +51,9 @@ def get_phoenix6_dependencies(
                 deps=[], artifact_install_name="CTRE_SimProCANcoder"
             ),
             "simProTalonFX": dict(deps=[], artifact_install_name="CTRE_SimProTalonFX"),
+            "simProTalonFXS": dict(
+                deps=[], artifact_install_name="CTRE_SimProTalonFXS"
+            ),
             "simTalonFX": dict(deps=[], artifact_install_name="CTRE_SimTalonFX"),
             "simTalonSRX": dict(deps=[], artifact_install_name="CTRE_SimTalonSRX"),
             "simVictorSPX": dict(deps=[], artifact_install_name="CTRE_SimVictorSPX"),
@@ -89,6 +92,7 @@ def get_phoenix6_dependencies(
                 "simProCANrange",
                 "simProPigeon2",
                 "simProTalonFX",
+                "simProTalonFXS",
                 "simTalonSRX",
                 "simVictorSPX",
                 "tools-sim",
@@ -106,6 +110,7 @@ def get_phoenix6_dependencies(
                 "simProCANrange",
                 "simProPigeon2",
                 "simProTalonFX",
+                "simProTalonFXS",
                 "simTalonSRX",
                 "simVictorSPX",
                 "tools-sim",
